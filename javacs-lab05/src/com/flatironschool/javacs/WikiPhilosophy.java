22a05fca5411905e2a25c5d9912b2836c5a0f22f
Lysette <isjochan@gmail.com>
2016-07-02 01:49:56 -0400

    completed lab (again)

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -14,6 +14,57 @@ public class WikiPhilosophy {
 	
 	final static WikiFetcher wf = new WikiFetcher();
 	
+	// gets first valid link on url
+	public static String crawl(String currentUrl) { 
+
+		String urlStart = "https://en.wikipedia.org";
+		int brackets = 0; // counts nesting of bracket
+		Elements paras;
+		
+		try {
+			paras = wf.fetchWikipedia(currentUrl);
+		
+			for (Element para : paras) { 
+				Iterable<Node> iter = new WikiNodeIterable(para);
+		
+				// go thru each node in each paragraph
+				for (Node node : iter) {
+					
+					// count brackets
+					if (node instanceof TextNode) {
+						String text = ((TextNode) node).text();
+						brackets += text.length() - text.replace("(", "").length();
+						brackets -= text.length() - text.replace(")", "").length();
+					}
+					
+					// check if node is link, not in italics (i or em), not in any brackets
+					if (node instanceof Element && 
+							((Element) node).tagName().equals("a") && 
+							!((Element) node.parent()).tagName().equals("i") &&
+							!((Element) node.parent()).tagName().equals("em") &&
+							brackets == 0) {
+						
+						// get link
+						String urlEnd = ((Element) node).attr("href");
+	
+						// no external links, or red links
+						// only internal non-red links start with /wiki/
+						if (urlEnd.startsWith("/wiki/")) { 
+							String url = urlStart + urlEnd;
+							// doesnt link back to current url
+							if (!url.equals(currentUrl))
+								return url;
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
 	 * 
@@ -32,20 +83,33 @@ public class WikiPhilosophy {
         // some example code to get you started
 
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		String urlEnd = "https://en.wikipedia.org/wiki/Philosophy";
 		Elements paragraphs = wf.fetchWikipedia(url);
 
+		/*
 		Element firstPara = paragraphs.get(0);
-		
+
 		Iterable<Node> iter = new WikiNodeIterable(firstPara);
 		for (Node node: iter) {
 			if (node instanceof TextNode) {
-				System.out.print(node);
+				System.out.println(node);
 			}
         }
+		*/
+		
+		List<String> urls = new ArrayList<String>();
+		
+		// stop if reach dead end, get to philosophy, or loop
+		while (url != null && !url.equals(urlEnd) && 
+				!urls.subList(0, Math.max(0, urls.size() - 2)).contains(url)) {
+			url = crawl(url);
+			System.out.println(url);
+			urls.add(url);
+		}
 
         // the following throws an exception so the test fails
         // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+        //String msg = "Complete this lab by adding your code and removing this statement.";
+        //throw new UnsupportedOperationException(msg);
 	}
 }
